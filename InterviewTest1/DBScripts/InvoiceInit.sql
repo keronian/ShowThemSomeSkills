9b0e11d51e6245e7c031b022bb2ad0ff60ebf703
keronian <keronian@yahoo.com>
2014-06-20 12:47:03 -0400

    Changed the syntax to be more DB agnostic.  Tested for MS SQL server as well as MySQL now.

--- InterviewTest1/DBScripts/InvoiceInit.sql
+++ InterviewTest1/DBScripts/InvoiceInit.sql
@@ -1,33 +1,34 @@
-CREATE TABLE "Invoice"
+CREATE TABLE Invoice
 (
-	"InvoiceNo" varchar(100) PRIMARY KEY,
-	"CompanyName" varchar(255),
+	InvoiceNo varchar(100) PRIMARY KEY,
+	CompanyName varchar(255),
 
 --	 These billing entries could easily be split into a separate table, but aren't here, which is fine, as there's no re-use
-	"BillingContact" varchar(255),
-	"BillingStreet" varchar(255),
-	"BillingCity" varchar(255),
-	"BillingState" varchar(2),
-	"BillingZip" varchar(10),
+	BillingContact varchar(255),
+	BillingStreet varchar(255),
+	BillingCity varchar(255),
+	BillingState varchar(2),
+	BillingZip varchar(10),
 
-	"PostedDate" datetime,
-	"ShippingDate" datetime,
-	"RequisitionDate" datetime,
-	"TaxRate" decimal(2,2),
-	"SubTotal" decimal(18,2),
-	"Shipping" decimal(18,2),
-	"Commission" decimal(18,2),
-	"Total" decimal(18,2)
+	PostedDate datetime,
+	ShippingDate datetime,
+	RequisitionDate datetime,
+	TaxRate decimal(2,2),
+	SubTotal decimal(18,2),
+	Shipping decimal(18,2),
+	Commission decimal(18,2),
+	Total decimal(18,2)
 );
 
-CREATE TABLE "InvoiceItem"
+CREATE TABLE InvoiceItem
 (
-	"InvoiceNo" varchar(100) FOREIGN KEY REFERENCES Invoice(InvoiceNo) ON DELETE CASCADE,
-	"LineText" varchar(255),
-	"Taxable" bit,
-	"Quantity" int,
-	"UnitPrice" decimal(18,2),
-	"Discount" tinyint,
-	"SubTotal" decimal(18,2),
-	"Total" decimal(18,2)
+	InvoiceNo varchar(100) NOT NULL,
+	LineText varchar(255),
+	Taxable bit,
+	Quantity int,
+	UnitPrice decimal(18,2),
+	Discount tinyint,
+	SubTotal decimal(18,2),
+	Total decimal(18,2),
+	FOREIGN KEY (InvoiceNo) References Invoice(InvoiceNo) ON DELETE CASCADE
 );
